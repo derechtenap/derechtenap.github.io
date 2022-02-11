a7fb730a5ca89229b604c52c65c2e97bfc26174d
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2022-02-11 18:11:20 +0100

    Create Footer.svelte

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,8 @@
+<footer
+  class="container mx-auto my-10 text-center text-xs text-zinc-400 dark:text-zinc-700 md:text-sm xl:text-lg"
+>
+  <p>
+    Diese Seite wurde mit <span class="text-red-500 dark:text-red-700">&hearts;</span> erstellt von Tim
+    Deres.
+  </p>
+</footer>
