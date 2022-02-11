19dd2fa291176fbeaf984ee22686b1df07ed646c
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2022-02-11 18:11:02 +0100

    Create index.svelte

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Nav from '$lib/Nav.svelte';
+  import Hero from '$lib/Hero.svelte';
+  import Footer from '$lib/Footer.svelte';
+  import { fade } from 'svelte/transition';
+</script>
+
+<svelte:head>
+  <title>Tim Deres &ndash; Frontend Web Developer</title>
+</svelte:head>
+
+<div class="h-screen" in:fade={{ delay: 2000, duration: 2000 }} out:fade={{ duration: 1000 }}>
+  <Nav />
+  <Hero />
+  <Footer />
+</div>
