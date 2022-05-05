494936c3c292907e12b89e731e8a918f986f35a3
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2022-05-05 18:27:40 +0200

    Extended the tailwindcss color palette

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        midnight: '#202531',
+        light: '#e3f4fc'
+      }
+    },
   },
   plugins: [],
 }
