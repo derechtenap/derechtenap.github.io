4649c0b6017d19307c90eead5dd965dfdb5b6774
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2022-05-22 23:29:51 +0200

    Add hero section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,4 +2,17 @@
   <title>Tim Deres &mdash; Frontend Web Developer</title>
 </svelte:head>
 
-INDEX
+<div class="hero is-fullheight-with-navbar">
+  <div class="hero-body">
+    <div class="container">
+      <p class="title is-1 pb-5">Hallo, ich bin Tim Deres.</p>
+      <p class="subtitle is-3">
+        Student @ Fachhochschule Südwestfalen in Hagen
+        <span class="has-text-link">&ndash;</span>
+        Wirtschaftsinformatik
+        <span class="has-text-link">&mdash;</span>
+        <b class="has-text-link">Frontend Web Developer</b>
+      </p>
+    </div>
+  </div>
+</div>
