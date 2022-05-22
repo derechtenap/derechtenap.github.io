bf0006a0229058bb053157f127186eba4df8cdca
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2022-05-22 23:30:11 +0200

    Add `Build with...` section

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,1 +1,19 @@
-<footer><!-- TODO :) --></footer>
+<script>
+  const currentYear = new Date().getFullYear();
+</script>
+
+<footer class="footer has-text-centered has-text-black">
+  <p class="has-text-grey title is-5">
+    Build with <a
+      href="https://bulma.io/"
+      target="_blank"
+      rel="noreferrer noopener">Bulma</a
+    >
+    and
+    <a
+      href="https://icons.getbootstrap.com/"
+      target="_blank"
+      rel="noreferrer noopener">Bootstrap Icons</a
+    >.
+  </p>
+</footer>
