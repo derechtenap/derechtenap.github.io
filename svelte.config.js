108e7dba2b1e5f91b4d72bf9fb72da4cbc5195d5
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2022-05-23 15:02:38 +0200

    Add adapter config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,20 @@
+import adapter from "@sveltejs/adapter-static";
+
+const dev = "production" === "development";
+
+// Guide: https://sveltesaas.com/articles/sveltekit-github-pages-guide/
+/** @type {import(""@sveltejs/kit").Config} */
 const config = {
-  kit: {},
+  kit: {
+    adapter: adapter({
+      pages: "docs",
+      assets: "docs"
+    }),
+    paths: {
+      // change below to your repo name
+      base: dev ? "" : "/derechtenap.github.io",
+    },
+  }
 };
 
 export default config;
