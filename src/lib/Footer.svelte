17717153ee727f22ae8ab20ff50d470381d3c195
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2022-05-24 17:20:17 +0200

    Rebuild entire footer component

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,19 +1,54 @@
 <script>
+  import ExternalLink from "./ExternalLink.svelte";
+
+  import bulmaLogo from "$lib/assets/png/madeWithBulma.png";
+  import footerIcons from "$lib/assets/json/footerIcons.json";
+
   const currentYear = new Date().getFullYear();
 </script>
 
-<footer class="footer has-text-centered has-text-black">
-  <p class="has-text-grey title is-5">
-    Build with <a
-      href="https://bulma.io/"
-      target="_blank"
-      rel="noreferrer noopener">Bulma</a
-    >
-    and
-    <a
-      href="https://icons.getbootstrap.com/"
-      target="_blank"
-      rel="noreferrer noopener">Bootstrap Icons</a
-    >.
+<footer class="footer has-text-centered">
+  {#each footerIcons as icon}
+    <ExternalLink link={icon.href}>
+      <i
+        class={`button is-large is-primary is-inverted bi-${icon.name.toLowerCase()}`}
+      />
+    </ExternalLink>
+  {/each}
+
+  <hr />
+
+  <p class="has-text-weight-semibold has-text-grey-darker mb-5">
+    &copy; {currentYear} &mdash; Erstellt von Tim Deres
   </p>
+
+  <small>
+    Created with
+    <ExternalLink link="https://kit.svelte.dev/">SvelteKit</ExternalLink>
+    and published using
+    <ExternalLink link="https://pages.github.com/">GitHub Pages</ExternalLink>.
+  </small>
+
+  <div class="container-logo mt-3">
+    <ExternalLink link="https://bulma.io">
+      <figure class="image">
+        <img src={bulmaLogo} alt="Logo des Bulma CSS-Frameworks" />
+      </figure>
+    </ExternalLink>
+  </div>
 </footer>
+
+<style>
+  .container-logo {
+    display: flex;
+    justify-content: center;
+  }
+  .image {
+    width: 163px;
+    height: 31px;
+  }
+
+  .button {
+    background-color: hsl(0deg, 0%, 98%) !important;
+  }
+</style>
