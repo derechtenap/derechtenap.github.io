1817b49ee36716e3435029ac8748bd1ba3c74ea2
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2022-06-08 12:33:37 +0200

    Moved 'Projekte' page to subfolder

---
+++ src/routes/projekte/index.svelte
@@ -0,0 +1,12 @@
+<script>
+  import PageTitle from "$lib/PageTitle.svelte";
+</script>
+<svelte:head>
+  <title>Projekte &ndash; Tim Deres &mdash; Frontend Web Developer</title>
+</svelte:head>
+
+<div class="container mb-5">
+  <PageTitle title="Meine Projekte">
+    An diesen Projekten habe ich in letzter Zeit gearbeitet.
+  </PageTitle>
+</div>
