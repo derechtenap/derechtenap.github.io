12c4bbb44933f776518a4b9fd3df7c49a97cbc97
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2022-06-08 12:38:06 +0200

    Add page title

--- src/routes/ueber-mich.svelte
+++ src/routes/ueber-mich.svelte
@@ -1,1 +1,16 @@
-<!-- TODO: :)-->
+<script>
+  import PageTitle from "$lib/PageTitle.svelte";
+</script>
+
+<svelte:head>
+  <title>Über mich &ndash; Tim Deres &mdash; Frontend Web Developer</title>
+</svelte:head>
+
+<div class="container mb-5">
+  <PageTitle title="Über mich">
+    Aktuell studiere ich <i>Wirtschaftsinformatik</i> an der Fachhochschule Südwestfalen.
+    Baue in meiner Freizeit kleinere Webprojekte und fahre nebenbei gerne auf den
+    Kartbahnen der Region im Kreis.
+  </PageTitle>
+
+</div>
