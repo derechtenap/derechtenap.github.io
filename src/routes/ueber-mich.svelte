72a3b490735a7e8fb4715007877b8a189d7c53cc
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2022-06-08 15:00:05 +0200

    Add new section
    Currently with placeholder text and image...

--- src/routes/ueber-mich.svelte
+++ src/routes/ueber-mich.svelte
@@ -14,4 +14,28 @@
     Kartbahnen der Region im Kreis.
   </PageTitle>
 
+  <div class="columns mt-6 is-align-items-center">
+    <div class="column is-5 line-height-text">
+      <p class="has-text-weight-bold is-size-5">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
+        alias nesciunt totam laborum nulla. Ab qui ullam odio numquam beatae!
+      </p>
+      <p class="my-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+        repudiandae ut dignissimos, inventore quam esse voluptas est et. Atque,
+        praesentium enim. Expedita dolores rerum, placeat minima error voluptate
+        quae sunt, molestiae iste in tempore qui.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
+        alias nesciunt totam laborum nulla. Ab qui ullam odio numquam beatae!
+      </p>
+    </div>
+    <div class="column">
+      <figure class="image">
+        <img src="https://bulma.io/images/placeholders/640x360.png" alt="" />
+      </figure>
+    </div>
+  </div>
+
 </Container>
