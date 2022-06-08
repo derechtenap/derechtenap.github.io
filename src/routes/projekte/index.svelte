be894d5864612144927cc84027232a44af42f9f4
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2022-06-08 13:06:02 +0200

    Add Container component
    Placed the code which is used on different pages into one component for easier maintenance

--- src/routes/projekte/index.svelte
+++ src/routes/projekte/index.svelte
@@ -1,12 +1,14 @@
 <script>
+  import Container from "$lib/Container.svelte";
   import PageTitle from "$lib/PageTitle.svelte";
 </script>
+
 <svelte:head>
   <title>Projekte &ndash; Tim Deres &mdash; Frontend Web Developer</title>
 </svelte:head>
 
-<div class="container mb-5">
+<Container>
   <PageTitle title="Meine Projekte">
     An diesen Projekten habe ich in letzter Zeit gearbeitet.
   </PageTitle>
-</div>
+</Container>
--- src/routes/ueber-mich.svelte
+++ src/routes/ueber-mich.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Container from "$lib/Container.svelte";
   import PageTitle from "$lib/PageTitle.svelte";
 </script>
 
@@ -6,11 +7,11 @@
   <title>Über mich &ndash; Tim Deres &mdash; Frontend Web Developer</title>
 </svelte:head>
 
-<div class="container mb-5">
+<Container>
   <PageTitle title="Über mich">
     Aktuell studiere ich <i>Wirtschaftsinformatik</i> an der Fachhochschule Südwestfalen.
     Baue in meiner Freizeit kleinere Webprojekte und fahre nebenbei gerne auf den
     Kartbahnen der Region im Kreis.
   </PageTitle>
 
-</div>
+</Container>
