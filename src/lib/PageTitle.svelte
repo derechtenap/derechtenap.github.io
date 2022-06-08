cfdb9d4e10afd41f60faf8f1b68df9da12fbc380
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2022-06-08 12:24:52 +0200

    Smaller subtitle on mobile devices
    In addition, the line height of the text was increased slightly.

--- src/lib/PageTitle.svelte
+++ src/lib/PageTitle.svelte
@@ -3,4 +3,4 @@
 </script>
 
 <h1 class="title is-1 is-spaced has-text-weight-bold my-5">{title}</h1>
-<p class="subtitle"><slot /></p>
+<p class="subtitle is-size-5-mobile line-height-text"><slot /></p>
