2e7a3d2c7b33161f3abcd4d56f521c4f26a0a8ce
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2022-06-11 15:00:40 +0200

    Add mdsvex and svelte-preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,8 @@
 import adapter from "@sveltejs/adapter-static";
+import sveltePreprocess from "svelte-preprocess";
+import {
+  mdsvex
+} from 'mdsvex';
 
 /** @type {import(""@sveltejs/kit").Config} */
 const config = {
@@ -12,7 +16,17 @@ const config = {
       assets: "https://derechtenap.github.io",
       base: "",
     },
-  }
+  },
+  extensions: ['.svelte', '.md'],
+  preprocess: [
+    sveltePreprocess(),
+    mdsvex({
+      extensions: ['.md'],
+      layout: {
+        projekte: 'src/routes/projekte/_projekt.svelte'
+      }
+    })
+  ]
 };
 
 export default config;
