0cd7797f9dfd73b53345d2b5338d0b154c70fd17
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2022-06-11 15:17:37 +0200

    Create Svelte API endpoint
    This endpoint is used to fetch all projects.

---
+++ src/routes/api/projekte.json.js
@@ -0,0 +1,29 @@
+export const get = async () => {
+    const allProjectFiles =
+        import.meta.glob('../projekte/*.md')
+    const iterableProjectFiles = Object.entries(allProjectFiles)
+
+    const allProjects = await Promise.all(
+        iterableProjectFiles.map(async ([path, resolver]) => {
+            const {
+                metadata
+            } = await resolver()
+
+            // Remove `..` and `.md` from the path
+            const projectPath = path.slice(2, -3)
+
+            return {
+                meta: metadata,
+                path: projectPath,
+            }
+        })
+    )
+
+    const sortedProjects = allProjects.sort((a, b) => {
+        return new Date(b.meta.date) - new Date(a.meta.date)
+    })
+
+    return {
+        body: sortedProjects
+    }
+}
