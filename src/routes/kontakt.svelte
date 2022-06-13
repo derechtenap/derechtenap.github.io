5ef216bcb8bf9fc048ed491c99a2c3c53406e154
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2022-06-13 21:14:55 +0200

    Moved form to the right column

--- src/routes/kontakt.svelte
+++ src/routes/kontakt.svelte
@@ -13,7 +13,7 @@
 <Container>
   <PageTitle title="Kontakt" />
   <div class="columns">
-    <div class="column is-6">
+    <div class="column is-6 mb-6">
       <p class="subtitle">Angaben gem. § 5 TMG:</p>
       <address class="subtitle is-5 line-height-text">
         Tim Deres <br />
@@ -28,41 +28,43 @@
           https://derechtenap.github.io
         </ExternalLink>
       </p>
-    </div>
-
-    <div class="column">
-      <p class="is-italic">Oder kontaktieren Sie mich über diese Dienste</p>
+      <p class="mt-6 mb-4">Oder kontaktieren Sie mich über diese Netzwerke:</p>
       {#each footerIcons as icon}
-        <i
-          class={`bi-${icon.name.toLowerCase()} is-size-2 has-text-primary mr-5`}
-        />
+        <ExternalLink link={icon.href}>
+          <i
+            class={`bi-${icon.name.toLowerCase()} is-size-2 has-text-primary mr-5`}
+            role="img"
+          />
+        </ExternalLink>
       {/each}
     </div>
-  </div>
-  <hr />
-  <div class="columns">
-    <form class="column is-6 pb-6">
-      <InputTextField id="name" label="Name" placeholder="Max Mustermann" />
-      <InputTextField
-        id="mail"
-        label="E-Mail-Adresse"
-        placeholder="max@mustermann.de"
-      >
-        Die E-Mail Adresse wird zur Kontaktaufnahme benötigt.
-      </InputTextField>
-      <InputTextField
-        id="subject"
-        label="Betreff"
-        placeholder="Anfrage wegen einer Webseite"
-      >
-        Worum geht es in Ihrer Anfrage? Ein neues Projekt? Haben Sie eine Frage?
-      </InputTextField>
 
-      <textarea class="textarea" placeholder="Ihre Nachricht..." required />
+    <aside class="column has-background-white-bis rounded px-6 py-5">
+      <form>
+        <InputTextField id="name" label="Name" placeholder="Max Mustermann" />
+        <InputTextField
+          id="mail"
+          label="E-Mail-Adresse"
+          placeholder="max@mustermann.de"
+        >
+          Die E-Mail Adresse wird zur Kontaktaufnahme benötigt.
+        </InputTextField>
+        <InputTextField
+          id="subject"
+          label="Betreff"
+          placeholder="Anfrage wegen einer Webseite"
+        >
+          Worum geht es in Ihrer Anfrage? Ein neues Projekt? Haben Sie eine
+          Frage?
+        </InputTextField>
 
-      <div class="control">
-        <button class="button is-primary mt-5" type="submit">Abschicken</button>
-      </div>
-    </form>
+        <textarea class="textarea" placeholder="Ihre Nachricht..." required />
+
+        <button class="button is-primary mt-5" type="submit">
+          <i class="bi-send mr-2" />
+          Abschicken
+        </button>
+      </form>
+    </aside>
   </div>
 </Container>
