819f4fb70dd434c49fde7fe5fef3c641ad79a8d3
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2022-06-13 21:15:13 +0200

    Create InputTextField.svelte

---
+++ src/lib/InputTextField.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let id, label, placeholder;
+</script>
+
+<div class="field mb-5">
+  <label class="label" for={id}>{label}</label>
+  <div class="control">
+    <input class="input" {id} {placeholder} required type="text" />
+  </div>
+  <p class="help mt-3">
+    <slot />
+  </p>
+</div>
