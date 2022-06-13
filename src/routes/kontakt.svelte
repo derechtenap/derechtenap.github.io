8438080475d66602330a937d86f8d9abe56aeb7f
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2022-06-13 18:20:17 +0200

    Add new page layout

--- src/routes/kontakt.svelte
+++ src/routes/kontakt.svelte
@@ -1,17 +1,27 @@
-<div class="hero is-fullheight-with-navbar">
-  <div class="hero-body is-align-items-start">
-    <div class="container">
-      <p class="title is-1 has-text-weight-bold pb-5">Kontakt</p>
-      <address class="subtitle is-5" style="line-height: 1.75em;">
+<script>
+  import Container from "$lib/Container.svelte";
+  import ExternalLink from "$lib/ExternalLink.svelte";
+  import PageTitle from "$lib/PageTitle.svelte";
+  import footerIcons from "$lib/assets/json/footerIcons.json";
+  import InputTextField from "$lib/InputTextField.svelte";
+</script>
+
+<Container>
+  <PageTitle title="Kontakt" />
+  <div class="columns">
+    <div class="column is-6">
+      <p class="subtitle">Angaben gem. § 5 TMG:</p>
+      <address class="subtitle is-5 line-height-text">
         Tim Deres <br />
         Auf der Ratschelle 15 <br />
         58840 Plettenberg
       </address>
 
+    </div>
 
 
 
       </div>
     </div>
   </div>
-</div>
+</Container>
