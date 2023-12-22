749a65a790d7c8daa3f2d0322e529dc3bae28b86
Tim <50623315+derechtenap@users.noreply.github.com>
2023-12-22 21:55:08 +0100

    Store posts images in the public folder

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -18,7 +18,10 @@ export default config({
           formatting: true,
           dividers: true,
           links: true,
-          images: true,
+          images: {
+            directory: "/public/posts",
+            publicPath: "/posts/"
+          }
         }),
       },
     }),
