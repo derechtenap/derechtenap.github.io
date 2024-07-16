689f95d93e9203070acd53ad51689a49e46828c2
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-07-16 14:16:54 +0200

    Prevent access to the admin dashboard in production

--- app/keystatic/layout.tsx
+++ app/keystatic/layout.tsx
@@ -1,5 +1,12 @@
-import KeystaticApp from './keystatic';
+import { showAdminUI } from "keystatic.config";
+import KeystaticApp from "./keystatic";
+import { notFound } from "next/navigation";
 
 export default function RootLayout() {
+  // Prevent access to the admin dashboard in production
+  if (!showAdminUI) {
+    notFound();
+  }
+
   return <KeystaticApp />;
 }
--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,27 +1,29 @@
-import { config, collection, fields } from '@keystatic/core';
-import React from "react"
+import { config, collection, fields } from "@keystatic/core";
+import React from "react";
+
+export const showAdminUI = process.env.NODE_ENV === "development";
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: "local",
   },
   collections: {
     posts: collection({
-      label: 'Posts',
-      slugField: 'title',
-      path: 'posts/*',
-      format: { contentField: 'content' },
+      label: "Posts",
+      slugField: "title",
+      path: "posts/*",
+      format: { contentField: "content" },
       schema: {
-        title: fields.slug({ name: { label: 'Title' } }),
+        title: fields.slug({ name: { label: "Title" } }),
         content: fields.document({
-          label: 'Content',
+          label: "Content",
           formatting: true,
           dividers: true,
           links: true,
           images: {
             directory: "/public/posts",
-            publicPath: "/posts/"
-          }
+            publicPath: "/posts/",
+          },
         }),
       },
     }),
@@ -30,7 +32,13 @@ export default config({
     // Brand icon and name for the `Keystatic` dashboard
     brand: {
       name: "Tim Deres",
-      mark: () => {return React.createElement("img", { alt: "Logo", height: 24, src: "/favicon.png", })}
-    }
-  }
+      mark: () => {
+        return React.createElement("img", {
+          alt: "Logo",
+          height: 24,
+          src: "/favicon.png",
+        });
+      },
+    },
+  },
 });
